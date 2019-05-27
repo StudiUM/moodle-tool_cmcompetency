124fc949865cf0f8279460ec556ce4d534dae8f3
Marie-Eve Levesque <marie-eve.levesque.8@umontreal.ca>
2019-05-27 15:47:59 -0400

    EVOSTDM-1567 - APC - Éval-comp-activité : Traduction en français

--- lang/en/tool_cmcompetency.php
+++ lang/en/tool_cmcompetency.php
@@ -26,13 +26,13 @@
 defined('MOODLE_INTERNAL') || die();
 
 $string['cannotaccessreportpage'] = 'Sorry, you do not have permissions to display this page.';
-$string['competencycmmenu'] = 'Competencies in course module';
-$string['errorinvalidcoursemodule'] = 'Invalid course module.';
-$string['eventusercompetencyratedincoursemodule'] = 'User competency rated in course module.';
-$string['eventusercompetencyviewedincoursemodule'] = 'User competency viewed in a course module.';
-$string['evidence_manualoverrideincoursemodule'] = 'The competency rating was manually set in the course module \'{$a}\'.';
-$string['jumptocm'] = 'Jump to course module';
-$string['nocompetenciesincm'] = 'No competencies have been linked to this course module.';
-$string['nocompetenciesincms'] = 'No competencies have been linked to course modules';
+$string['competencycmmenu'] = 'Competencies linked in the course activities';
+$string['errorinvalidcoursemodule'] = 'Invalid course activity.';
+$string['eventusercompetencyratedincoursemodule'] = 'User competency rated in course activity.';
+$string['eventusercompetencyviewedincoursemodule'] = 'User competency viewed in a course activity.';
+$string['evidence_manualoverrideincoursemodule'] = 'The competency rating was manually set in the course activity \'{$a}\'.';
+$string['jumptocm'] = 'Jump to course activity';
+$string['nocompetenciesincm'] = 'No competencies have been linked to this course activity.';
+$string['nocompetenciesincms'] = 'No competencies have been linked to course activities';
 $string['pluginname'] = 'Course module competency rating';
-$string['xcompetenciesproficientoutofyincoursemodule'] = 'You are proficient in {$a->x} out of {$a->y} competencies in this course module.';
+$string['xcompetenciesproficientoutofyincoursemodule'] = 'You are proficient in {$a->x} out of {$a->y} competencies in this course activity.';
